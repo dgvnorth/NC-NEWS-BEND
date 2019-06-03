cf0bc19424f7189296143c8f4e003fae46dca6f1
dgvnorth <campello03@hotmail.com>
2019-06-03 14:26:20 +0100

    topics and users data seeded

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,10 +1,32 @@
-// const {  } = require('../data');
+const {
+  topicsData,
+  usersData,
+  articlesData,
+  commentsData
+} = require("../data");
 
 exports.seed = (knex, Promise) => {
   return knex.migrate
     .rollback()
     .then(() => knex.migrate.latest())
     .then(() => {
-      // insert data
+      return knex("topics")
+        .insert(topicsData)
+        .returning("*");
+    })
+    .then(() => {
+      return knex("users")
+        .insert(usersData)
+        .returning("*");
+    })
+    .then(topicsData => {
+      const houseRef = createRef(houseRows, "house_name", "house_id");
+      const formattedWizards = formatWizards(wizardData, houseRef);
+      return knex("wizards")
+        .insert(formattedWizards)
+        .returning("*");
+    })
+    .then(usersData => {
+      console.log(usersData); // to see the tables
     });
 };
